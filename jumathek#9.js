0a33b9f2b311b125303dd8a161f0ecf3e218e0f8
Muhammad Aji Raharjo <loroyangtau@gmail.com>
2020-01-04 20:30:43 +0700

    Add explanation on code

--- jumathek#9.js
+++ jumathek#9.js
@@ -1,19 +1,24 @@
 minesweeper = (block) => {
+    // Split the string first
     block = block.split(" ")
     let mineCounted = 0
-    // console.log(block.length)
+
     for ( let i in block ) {
-        // console.log(mineCounted)
+        // Since it zero should be no mine around
         if (block[i] === "0") {
             continue;
         } else if (block[i] === "1") {
+            // if it on first block and next block not 1 there should be mine
             if (i === "0" && block[parseInt[i]+1] !== "1") {
                 mineCounted += 1
+            // if it on last block and previous block not 1 there should be mine
             } else if (parseInt(i) === (block.length-1) && block[parseInt(i)-1] !== "1") {
                 mineCounted += 1
+            // if next block is 2 or 1 there should be mine
             } else if (block[parseInt(i)+1] === "1" || block[parseInt(i)+1] === "2") {
                 mineCounted += 1
             } 
+            // if current block is 2 and next block is 2 or 1 there should be mine
         } else if (block[i] === "2" && (block[parseInt(i)+1] === "1" || block[parseInt(i)+1] === "2")) {
             mineCounted += 1
         }
@@ -23,5 +28,5 @@ minesweeper = (block) => {
 }
 
 console.log(minesweeper("0 1 1 0 0 0 0")) // 1
-console.log(minesweeper("1 0 0 0 0 0 1 2 1 0")) // 1
-console.log(minesweeper("0 0 0 0 0 0 0 0 0 0")) // 1
+console.log(minesweeper("1 0 0 0 0 0 1 2 1 0")) // 3
+console.log(minesweeper("0 0 0 0 0 0 0 0 0 0")) // 0
